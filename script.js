836699ecce89396855facb088a2fc200196b8502
Aditi0090 <165465425+Aditi0090@users.noreply.github.com>
2024-12-27 22:04:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// //your JS code here. If required.
-// document.getElementById("fname").addEventListener("blur", function() {
-//     this.value = this.value.toUpperCase();
-// });
+//your JS code here. If required.
+document.getElementById("fname").addEventListener("blur", function() {
+    this.value = this.value.toUpperCase();
+});
